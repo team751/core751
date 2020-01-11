a23a98d5a706dfe9f8d23c5d1d6cea0936610181
Bobby Youstra <bobbyyoustra@protonmail.com>
2020-01-11 14:05:54 -0800

    Update Camera.java

--- subsystems/Camera.java
+++ subsystems/Camera.java
@@ -1,14 +1,25 @@
 package frc.robot.core751.subsystems;
 
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Camera extends SubsystemBase {
+    int m_activeDevice;
+    UsbCamera[] m_usbCameras;
+
     public Camera(int device) {
-        CameraServer.getInstance().startAutomaticCapture(device);
+        m_activeDevice = device;
+        m_usbCameras = new UsbCamera[UsbCamera.enumerateUsbCameras().length];
+
+        m_usbCameras[device] = CameraServer.getInstance().startAutomaticCapture("USB Camera 0", device);
     }
 
     public void switchCamera(int toDeviceNum) {
-        CameraServer.getInstance().startAutomaticCapture(toDeviceNum);
+        if(m_usbCameras[toDeviceNum] != null) {
+            CameraServer.getInstance().startAutomaticCapture(m_usbCameras[toDeviceNum]);
+        } else {
+            m_usbCameras[toDeviceNum] = CameraServer.getInstance().startAutomaticCapture("USB Camera 0", toDeviceNum);
+        }
     }
 }
