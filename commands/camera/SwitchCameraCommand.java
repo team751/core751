eb59a570d4f3931dcc9d53954c15e4d36506fe7f
Bobby Youstra <bobbyyoustra@protonmail.com>
2020-01-11 12:09:00 -0800

    Update SwitchCameraCommand.java

--- commands/camera/SwitchCameraCommand.java
+++ commands/camera/SwitchCameraCommand.java
@@ -7,7 +7,6 @@ import frc.robot.core751.subsystems.Camera;
 public class SwitchCameraCommand extends CommandBase {
     private int m_device;
     private Camera m_cameraSubsystem;
-    private boolean m_finished = false;
 
     public SwitchCameraCommand(Camera cameraSubsystem, int initialDevice) {
         m_device = initialDevice;
@@ -18,15 +17,21 @@ public class SwitchCameraCommand extends CommandBase {
     }
 
     public void initialize() {
+        int cameraNum = UsbCamera.enumerateUsbCameras().length;
+        
+        if(m_device + 1 <= cameraNum) {
+            m_device = 0;
+        } else {
+            ++m_device;
+        }
+        
         m_cameraSubsystem.switchCamera(m_device);
 
         SmartDashboard.putNumber("Camera Device Number", m_device);
-
-        m_finished = true;
     }
 
     public boolean isFinished() {
-        return m_finished;
+        return true;
     }
 
     public int getDeviceNumber() {
